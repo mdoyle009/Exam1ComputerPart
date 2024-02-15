5eba3c02340163d9cd659c92270c997d714b521f
Matthew Doyle <mdoyle009@student.franciscan.edu>
2024-02-15 11:31:21 -0500

    finished m1

--- src/m1_movie_review.py
+++ src/m1_movie_review.py
@@ -1,5 +1,5 @@
 ###############################################################################
-# TODO: 1. (3 pts)
+# DONE: 1. (3 pts)
 #
 #   In this module, we are going to create a program that will allow a user to
 #   give a review for a movie.
@@ -17,8 +17,12 @@
 #   Once you have done this, then change the above _TODO_ to DONE.
 ###############################################################################
 
+def movie():
+    movie_title = input("Please enter a movie title: ")
+    return movie_title
+
 ###############################################################################
-# TODO: 2. (4 pts)
+# DONE: 2. (4 pts)
 #
 #   Next, write a function called rating() that will prompt the user to enter a
 #   rating for the movie and return it as a float.
@@ -32,8 +36,13 @@
 #   Once you have done this, then change the above _TODO_ to DONE.
 ###############################################################################
 
+def rating():
+    raw_movie_rating = input("Please enter a movie rating (1-5): ")
+    movie_rating = float(raw_movie_rating)
+    return movie_rating
+
 ###############################################################################
-# TODO: 3. (3 pts)
+# DONE: 3. (3 pts)
 #
 #   Now, write a function called review() that will prompt the user to enter a
 #   short review of the movie return the review text.
@@ -48,8 +57,12 @@
 #   Once you have done this, then change the above _TODO_ to DONE.
 ###############################################################################
 
+def review():
+    movie_review = input("Please enter a brief review: ")
+    return movie_review
+
 ###############################################################################
-# TODO: 4. (7 pts)
+# DONE: 4. (7 pts)
 #
 #   Now, let's put it all together.
 #
@@ -73,3 +86,11 @@
 #
 #   Once you have done this, then change the above _TODO_ to DONE.
 ###############################################################################
+
+def main():
+    print("Please write your review below.")
+    titleVar = movie()
+    ratingVar = rating()
+    reviewVar = review()
+    print(f"Movie: {titleVar} \nRating: {ratingVar}/5 \nReview: {reviewVar}")
+main()
